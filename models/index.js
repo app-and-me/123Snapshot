73a4f9a41d4e837467d05fa522eb37dffd1467f1
dayul <dyulchoo@naver.com>
2024-05-21 00:18:51 +0900

    error : table 생성 안됨

--- models/index.js
+++ models/index.js
@@ -8,12 +8,12 @@ const sequelize = require('../config/config')
 // const Diary = require('./diary')(sequelize);// sequelize가 db 정보를 가지고 있으므로 전달해주면 얘가 만들어봄 
 // const SelfTestResults = require('./self-test-result')(sequelize);// sequelize가 db 정보를 가지고 있으므로 전달해주면 얘가 만들어봄 
 
-const imagePathRouter = require('./routes/image_paths')(sequelize);
-//const titlesRouter = require('./routes/titles')(sequelize);
-const lettersRouter = require('./routes/letters')(sequelize);
+const imagePathRouter = require('../routes/image_paths')(sequelize);
+const titlesRouter = require('../routes/titles')(sequelize);
+const lettersRouter = require('../routes/letters')(sequelize);
 
 module.exports = {
   imagePathRouter,
-  //titlesRouter,
+  titlesRouter,
   lettersRouter,
 }
