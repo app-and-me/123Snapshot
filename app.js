f6d3e0c406ae919f06540fa6a77dc6c1a271c684
dayul <dyulchoo@naver.com>
2024-05-20 21:10:32 +0900

    error : 테이블 생성

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
 const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
+const multer = require('multer');
+const sequelize = require("./config/config");
+
+
+const path = require('path');
+const bodyParse = require('body-parser')
+
 const app = express();
 const port = 3000;
 
 // routes 만든 라우터 불러오기
-const mainRouter = require('./routes/image_paths');
-const mainRouter = require('./routes/titles');
-const mainRouter = require('./routes/letters');
+const imagePathRouter = require('./routes/image_paths');
+//const titlesRouter = require('./routes/titles');
+const lettersRouter = require('./routes/letters');
+
+sequelize.sync()
+  .then(() => {
+    console.log('Database synced')
+  })
+  .catch((err) => {
+    console.error('Error syncing database:', err)
+  });
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(require('cookie-parser')());
+app.use(bodyParse.json())
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true,
+}));
+  
 
 // router
-app.use('/image_path', image_paths);
-app.use('/titles', titles);
-app.use('/lettes', letters);
+app.use('/image_path', imagePathRouter);
+//app.use('/titles', titlesRouter);
+app.use('/letters', lettersRouter);
 
 // 서버 실행
 app.listen(port, () => {
     console.log(`${port}에서 서버 실행`);
 })
+
