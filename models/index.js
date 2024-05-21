754af98c15c148940bd316597a1541312ef917a9
dayul <dyulchoo@naver.com>
2024-05-21 20:47:28 +0900

    fix : table 생성 오류 해결

--- models/index.js
+++ models/index.js
@@ -2,18 +2,12 @@
 // const로 상수 선언 
 const sequelize = require('../config/config')
 
-// const User = require('./user')(sequelize);// sequelize가 db 정보를 가지고 있으므로 전달해주면 얘가 만들어봄 
-// const Question = require('./question')(sequelize);// sequelize가 db 정보를 가지고 있으므로 전달해주면 얘가 만들어봄 
-// const Company = require('./company')(sequelize);// sequelize가 db 정보를 가지고 있으므로 전달해주면 얘가 만들어봄 
-// const Diary = require('./diary')(sequelize);// sequelize가 db 정보를 가지고 있으므로 전달해주면 얘가 만들어봄 
-// const SelfTestResults = require('./self-test-result')(sequelize);// sequelize가 db 정보를 가지고 있으므로 전달해주면 얘가 만들어봄 
-
-const imagePathRouter = require('../routes/image_paths')(sequelize);
-const titlesRouter = require('../routes/titles')(sequelize);
-const lettersRouter = require('../routes/letters')(sequelize);
+// const imagePathRouter = require('../routes/image_paths')(sequelize);
+// const titlesRouter = require('../routes/titles')(sequelize);
+const letter = require('./letters')(sequelize);
 
 module.exports = {
-  imagePathRouter,
-  titlesRouter,
-  lettersRouter,
+  // imagePathRouter,
+  // titlesRouter,
+  letter
 }
--- routes/letters.js
+++ routes/letters.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {Letter} = require('../models/letters');
+const { Letter } = require('../models');
+// table 정의되면 여기도 불러와야함
 
 // 예시 코드
 router.get("/letters/:id", (req,res) => {
