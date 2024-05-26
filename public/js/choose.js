51f9e0573adac767a5010efdc1f59879c7acecbd
배주연 <w2316@e-mirim.hs.kr>
2024-05-26 15:09:42 +0900

    fix : 다음 페이지로 넘어가기

---
+++ public/js/choose.js
@@ -0,0 +1,13 @@
+function publicNextPage(event){
+    event.preventDefault(); // 폼의 기본 제출 동작을 막음
+    var form = document.getElementById('myForm');
+    form.submit(); // 폼을 제출
+    window.location.href = 'post.html'; // 다음 페이지로 넘어가기
+}
+
+function privateNextPage(event){
+    event.preventDefault();
+    var from = document.getElementById('myForm');
+    form.submit();
+    window.location.href = '#';
+}
--- public/js/write.js
+++ public/js/write.js
@@ -1,27 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
-    const writingBox = document.getElementById('writing-box');
-    const placeholderPart1 = '과거의 나에게 한마디를 남긴다면?';
-    const placeholderPart2 = '예시: 걱정하지 마, 잘될 거야~';
-    const placeholderHTML = `${placeholderPart1}<div>${placeholderPart2}</div>`;
-
-    const addPlaceholder = (box, placeholderHTML) => {
-        if (!box.innerHTML.trim() || box.innerHTML === '<br>') {
-            box.innerHTML = placeholderHTML;
-            box.classList.add('placeholder');
-        }
-    };
-
-    const removePlaceholder = (box, placeholderHTML) => {
-        if (box.innerHTML.trim() === placeholderHTML) {
-            box.innerHTML = '';
-            box.classList.remove('placeholder');
-        }
-    };
-
-    writingBox.addEventListener('focus', () => removePlaceholder(writingBox, placeholderHTML));
-    writingBox.addEventListener('blur', () => addPlaceholder(writingBox, placeholderHTML));
-
-    // Initialize the placeholders
-    addPlaceholder(writingBox, placeholderHTML);
-});
+function nextPage(event) {
+    event.preventDefault(); // 폼의 기본 제출 동작을 막음
+    document.forms[0].submit(); // 폼을 제출
+    window.location.href = 'index.html'; // 페이지 리디렉션
+}
