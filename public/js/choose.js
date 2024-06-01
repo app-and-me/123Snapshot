a6272806145b7ba3374eaeab5d0fd9b2573e2785
배주연 <w2316@e-mirim.hs.kr>
2024-06-01 16:25:49 +0900

    fix : 링크 경로 수정

--- public/js/choose.js
+++ public/js/choose.js
@@ -2,12 +2,12 @@ function publicNextPage(event){
     event.preventDefault(); // 폼의 기본 제출 동작을 막음
     var form = document.getElementById('myForm');
     form.submit(); // 폼을 제출
-    window.location.href = 'post.ejs'; // 다음 페이지로 넘어가기
+    window.location.href = '/post'; // 다음 페이지로 넘어가기
 }
 
 function privateNextPage(event){
     event.preventDefault();
     var from = document.getElementById('myForm');
     form.submit();
-    window.location.href = '#';
+    window.location.href = '/notpost';
 }
