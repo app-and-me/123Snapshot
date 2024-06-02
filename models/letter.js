f3cb0aba02ca27a1c5a52c697922eb5c347e22a9
dayul <dyulchoo@naver.com>
2024-06-03 08:32:12 +0900

    fix : sequelize 관련 에러 해결

---
+++ models/letter.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require('sequelize');
+// const sequelize = require('./index');
+
+const Letter = (sequelize) => sequelize.define('letters', {
+  id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,       // 기본 키로 설정
+    autoIncrement: true,
+    allowNull: false
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  image_paths: {
+    type: DataTypes.STRING(255),
+    allowNull: true
+  },
+  titles: {
+    type: DataTypes.STRING(255),
+    allowNull: true
+  },
+  yn: {
+    type: DataTypes.BOOLEAN,
+    allowNull: true
+  }
+},
+{
+timestamps: false,    // 기본으로 생기는 column을 생기지 않게 함
+indexes: [
+  {
+    unique: true,
+    fields: ['userId']    // user별 고유성을 보장
+  }
+]
+});
+ 
+module.exports = Letter;
+  
