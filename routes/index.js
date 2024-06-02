b11df2334cb924116c4397fe700353d7dcabc6eb
dayul <dyulchoo@naver.com>
2024-06-03 00:25:09 +0900

    feat : 사용자 id 관련 기능 추가

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const {Letter} = require('../models');
 const { Op } = require('sequelize');
 
 const router = express.Router();
+
+let currentUserId = 1;    // 현재 사용자 id 
 
 // 처음 화면 시작
 router.get('/', (req, res) => {
@@ -9,9 +12,36 @@ router.get('/', (req, res) => {
 });
 
 // index.ejs에서 story로 이동
-router.get('/story', (req, res) =>{
+router.get('/story', (req, res) => {
   res.render('story');
-})
+});
+
+// 사용자 id 생성, 프론트로 전달
+router.get('/newUserId', (req, res) => {
+  res.json({ userId: currentUserId });   // 사용자 id 프론트로 보내기
+  currentUserId += 1;   // 사용자 ID 증가
+});
+
+// 사용자 id값 DB에 저장
+router.post('/saveUserId', async (req, res) => {
+  try {
+      const userId = req.body.userId;
+
+      const user = await Letter.create({
+          userId: userId
+      });
+
+      if (user) {
+          res.status(200).json({ "message":"사용자 ID 저장 성공" });
+      } else {
+          res.status(500).json({ "message":"사용자 ID 저장 실패" });
+      }
+  }
+  catch (error) {
+      console.log(error);
+      res.status(500).json({ "message":"서버 오류로 사용자 ID 저장 실패" });
+  }
+});
 
 // story에서 letter으로 이동
 router.get('/letter', (req, res) =>{
@@ -58,9 +88,7 @@ router.post('/choose', (req, res) => {
 
 // /post, /notpost으로 이동 3초 후 index으로 이동
 router.get(['/post', '/notpost'], (req, res) => {
-    setTimeout(function() {
-        res.render('index');
-    }, 3000);
+  res.render('index');
 });
 
 // app.js에서 사용할 수 있도록 내보냄
