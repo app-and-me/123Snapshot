7cfa55cfbcf121cc68dccee62cf1a3ccc95ed1d4
배주연 <w2316@e-mirim.hs.kr>
2024-06-09 04:45:15 +0900

    fix : ejs 파일 리다이렉트 추가

--- routes/index.js
+++ routes/index.js
@@ -68,11 +68,28 @@ router.get('/write', (req, res) =>{
   res.render('write');
 })
 
+router.get('/choose', (req, res)=>{
+  res.render('choose')
+})
+
+
+router.get('/post', (req, res) => {
+  res.render('post');
+});
+
+
+router.get('/notpost', (req, res) => {
+  res.render('notpost');
+});
+
+
+
 // choose에서 yes/no -> /post, /notpost으로 이동
-router.post('/choose', (req, res) => {
+router.post('/choose', (req, res) => { //res.render choose
+   
     try {
         const {public} = req.body;
-    
+    console.log(public)
         if(public === 'yes') {
             res.render('post');
         }
@@ -91,9 +108,9 @@ router.post('/choose', (req, res) => {
 
 })
 
-// /post, /notpost으로 이동 3초 후 index으로 이동
-router.get(['/post', '/notpost'], (req, res) => {
-  res.render('index');
+
+router.get('/board', (req, res) => {
+  res.render('board');
 });
 
 // app.js에서 사용할 수 있도록 내보냄
